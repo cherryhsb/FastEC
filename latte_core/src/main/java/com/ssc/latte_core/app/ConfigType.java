81b0d4c8f4451f8a1aa1ee5fa4ecfda99c357650
Cherryhsb <suicoder@foxmail.com>
2018-08-29 03:09:31 +0800

    1.5.6 finish 大致完成RestClient 2.ConfigType忘记添加,现在已经添加

---
+++ latte_core/src/main/java/com/ssc/latte_core/app/ConfigType.java
@@ -0,0 +1,17 @@
+package com.ssc.latte_core.app;
+
+/*
+ *  项目名：    FastEC
+ *  包名:       com.ssc.latte_core.app
+ *  文件名：    ConfigType
+ *  创建者：    SSC
+ *  创建时间：   2018/8/25 17:23
+ *  描述：     TODO
+ */
+
+public enum ConfigType {
+    API_HOST,//网络请求域名等
+    APPLICATION_CONTEXT,//全局上下文
+    CONFIG_READY,//初始化或配置是否完成
+    ICON//自己的初始化项目
+}
