7b279410a61f4280265a6558adccd0d04929dd24
Cherryhsb <suicoder@foxmail.com>
2018-09-03 01:28:10 +0800

    5.7 finish 完善RestClient

--- latte_core/src/main/java/com/ssc/latte_core/net/RestClient.java
+++ latte_core/src/main/java/com/ssc/latte_core/net/RestClient.java
@@ -21,8 +21,11 @@ import com.ssc.latte_core.net.callback.RequestCallbacks;
 import com.ssc.latte_core.ui.LatteLoader;
 import com.ssc.latte_core.ui.LoaderStyle;
 
+import java.io.File;
 import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,15 +39,16 @@ public class RestClient {
     private final IFailure FAILURE;
     private final IError ERROR;
     private final RequestBody BODY;
-    private final Context CONTEXT;
+    private final File FILE;
+    private final Context CONTEXT;  //传入的当前上下文  显示loading用
     private final LoaderStyle LOADER_STYLE;
 
 
     public RestClient(String url, Map<String, Object> params,
                       IRequest request, ISuccess success,
                       IFailure failure, IError error,
-                      RequestBody body,
-                      Context context,LoaderStyle loaderStyle) {
+                      RequestBody body, File file,
+                      Context context, LoaderStyle loaderStyle) {
         this.URL = url;
         PARAMS.putAll(params);
         this.REQUEST = request;
@@ -52,6 +56,7 @@ public class RestClient {
         this.FAILURE = failure;
         this.ERROR = error;
         this.BODY = body;
+        this.FILE = file;
         this.CONTEXT = context;
         this.LOADER_STYLE = loaderStyle;
 
@@ -70,9 +75,9 @@ public class RestClient {
             REQUEST.onRequestStart();
         }
 
-        if (LOADER_STYLE!=null){
+        if (LOADER_STYLE != null) {
             //显示loading
-            LatteLoader.showLoading(CONTEXT,LOADER_STYLE);
+            LatteLoader.showLoading(CONTEXT, LOADER_STYLE);
         }
 
         switch (method) {
@@ -82,11 +87,26 @@ public class RestClient {
             case POST:
                 call = service.post(URL, PARAMS);
                 break;
+            case POST_RAW:
+                call = service.postRaw(URL, BODY);
+                break;
             case PUT:
                 call = service.put(URL, PARAMS);
                 break;
+            case PUT_RAW:
+                call = service.putRaw(URL, BODY);
+                break;
             case DELETE:
                 call = service.delete(URL, PARAMS);
+                break;
+            case UPLOAD:
+                final RequestBody requestBody = RequestBody.
+                        create(MediaType.parse(MultipartBody.FORM.toString()), FILE);
+                final MultipartBody.Part part = MultipartBody.Part.
+                        createFormData("file", FILE.getName(), requestBody);
+                // TODO: 2018/9/3
+                //?为什么不直接用已有的service.upload    而是用RestCreator.getRestService()再实例化一个service
+                call = RestCreator.getRestService().upload(URL, part);
                 break;
             default:
                 break;
@@ -99,25 +119,39 @@ public class RestClient {
     }
 
     private Callback<String> getRequestCallback() {
-        return new RequestCallbacks(REQUEST, SUCCESS, FAILURE, ERROR,LOADER_STYLE);
+        return new RequestCallbacks(REQUEST, SUCCESS, FAILURE, ERROR, LOADER_STYLE);
     }
 
     /*具体的使用方法
-    * get,post,put,delete
-    * */
-    public final void get(){
+     * get,post,put,delete
+     * */
+    public final void get() {
         request(HttpMethod.GET);
     }
 
-    public final void post(){
-        request(HttpMethod.POST);
+    public final void post() {
+        if (BODY == null) {
+            request(HttpMethod.POST);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.POST_RAW);
+        }
     }
 
-    public final void put(){
-        request(HttpMethod.PUT);
+    public final void put() {
+        if (BODY == null) {
+            request(HttpMethod.PUT);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.PUT_RAW);
+        }
     }
 
-    public final void delete(){
+    public final void delete() {
         request(HttpMethod.DELETE);
     }
 }
--- latte_core/src/main/java/com/ssc/latte_core/net/RestService.java
+++ latte_core/src/main/java/com/ssc/latte_core/net/RestService.java
@@ -12,8 +12,10 @@ package com.ssc.latte_core.net;
 import java.util.Map;
 
 import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
@@ -35,9 +37,17 @@ public interface RestService {
     @POST
     Call<String> post(@Url String url, @FieldMap Map<String, Object> params);
 
+    //原始数据不能加@FormUrlEncoded
+    @POST
+    Call<String> postRaw(@Url String url, @Body RequestBody body);
+
     @FormUrlEncoded
     @PUT
     Call<String> put(@Url String url, @FieldMap Map<String, Object> params);
+
+    //原始数据不能加@FormUrlEncoded
+    @PUT
+    Call<String> putRaw(@Url String url, @Body RequestBody body);
 
     @DELETE
     Call<String> delete(@Url String url, @QueryMap Map<String, Object> params);
